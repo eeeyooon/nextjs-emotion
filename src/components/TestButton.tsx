21f02d5e70311161c869fda621839d7d4c6647f8
jiyoon <vywns9978@gmail.com>
2024-02-25 17:32:38 +0900

    스타일드 컴포넌트에 props를 기반으로 스타일링 설정

--- src/components/TestButton.tsx
+++ src/components/TestButton.tsx
@@ -3,12 +3,18 @@ import styled from "@emotion/styled";
 type Props = {
   message: string;
 };
+
+type StyledProps = {
+  color: string;
+};
+
 export default function TestButton({ message }: Props) {
-  return <StyledButton>{message}</StyledButton>;
+  return <StyledButton color="primary">{message}</StyledButton>;
 }
 
-const StyledButton = styled.button`
-  background-color: rebeccapurple;
+const StyledButton = styled.button<StyledProps>`
+  background-color: ${({ color }) =>
+    color === "primary" ? "rebeccapurple" : "blue"};
   color: white;
   font-size: 16px;
   padding: 10px 20px;
